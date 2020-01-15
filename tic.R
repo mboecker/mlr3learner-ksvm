c3402458810be766129d1203ae25554d45e95d3e
pat-s <patrick.schratz@gmail.com>
2020-01-15 23:24:02 +0100

    add tic infrastructure and try drat deployment

---
+++ tic.R
@@ -0,0 +1,15 @@
+# installs dependencies, runs R CMD check, runs covr::codecov()
+do_package_checks()
+
+# build drat package
+get_stage("before_deploy") %>%
+  add_step(step_setup_ssh())
+
+get_stage("deploy") %>%
+  add_step(step_setup_push_deploy(
+    path = "~/git/drat",
+    branch = "master",
+    remote = paste0("git@github.com:", gsub("/.*$", "/mlr3learners.drat", ci()$get_slug()), ".git")
+  )) %>%
+  add_step(step_add_to_drat()) %>%
+  add_step(step_do_push_deploy(path = "~/git/drat"))
