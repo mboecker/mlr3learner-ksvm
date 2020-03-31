aa483e5c70541521f9ddbab98600a38dc8db2fef
pat-s <patrick.schratz@gmail.com>
2020-03-31 21:58:16 +0200

    lint

--- inst/paramtest/test_paramtest_regr_ksvm.R
+++ inst/paramtest/test_paramtest_regr_ksvm.R
@@ -4,10 +4,11 @@ test_that("regr.ksvm", {
   learner = lrn("regr.ksvm")
   fun = kernlab::ksvm
   exclude = c(
-      "x", # handled by mlr3
-      "..." # not supported
+    "x", # handled by mlr3
+    "..." # not supported
   )
   ParamTest = run_paramtest(learner, fun, exclude)
-  expect_true(ParamTest, info = paste0("\nMissing parameters:\n",
-    paste0("- '", ParamTest$missing,"'", collapse = "\n")))
+  expect_true(ParamTest, info = paste0(
+    "\nMissing parameters:\n",
+    paste0("- '", ParamTest$missing, "'", collapse = "\n")))
 })
