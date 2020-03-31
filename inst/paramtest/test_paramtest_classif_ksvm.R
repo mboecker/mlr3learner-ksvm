af11cb2eac0d1f1a2e098d7f6f4257479af0be54
pat-s <patrick.schratz@gmail.com>
2020-03-31 15:46:13 +0200

    add Paramtest

---
+++ inst/paramtest/test_paramtest_classif_ksvm.R
@@ -0,0 +1,13 @@
+library(mlr3learners.kernlab)
+
+test_that("classif.ksvm", {
+  learner = lrn("classif.ksvm")
+  fun = kernlab::ksvm
+  exclude = c(
+      "x", # handled by mlr3
+      "..." # not supported
+  )
+  ParamTest = run_paramtest(learner, fun, exclude)
+  expect_true(ParamTest, info = paste0("\nMissing parameters:\n",
+    paste0("- '", ParamTest$missing,"'", collapse = "\n")))
+})
---
+++ inst/paramtest/test_paramtest_regr_ksvm.R
@@ -0,0 +1,13 @@
+library(mlr3learners.kernlab)
+
+test_that("regr.ksvm", {
+  learner = lrn("regr.ksvm")
+  fun = kernlab::ksvm
+  exclude = c(
+      "x", # handled by mlr3
+      "..." # not supported
+  )
+  ParamTest = run_paramtest(learner, fun, exclude)
+  expect_true(ParamTest, info = paste0("\nMissing parameters:\n",
+    paste0("- '", ParamTest$missing,"'", collapse = "\n")))
+})
