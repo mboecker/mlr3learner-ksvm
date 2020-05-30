7951704ce0b14f1c16027719a0063f66b92207b4
pat-s <patrick.schratz@gmail.com>
2020-05-30 16:41:27 +0200

    try drat eployment

--- tic.R
+++ tic.R
@@ -1,8 +1,6 @@
 # R CMD check
 if (!ci_has_env("PARAMTEST")) {
   do_package_checks()
-
-  do_drat("mlr3learners/mlr3learners.drat")
 } else {
   # PARAMTEST
   get_stage("install") %>%
@@ -10,6 +8,14 @@ if (!ci_has_env("PARAMTEST")) {
 
   get_stage("script") %>%
     add_code_step(remotes::install_dev("mlr3")) %>%
-    add_code_step(testthat::test_dir(system.file("paramtest", package = "mlr3learners.kernlab"),
-      stop_on_failure = TRUE))
+    add_code_step(testthat::test_dir(system.file("paramtest",
+      package = "mlr3learners.kernlab"),
+    stop_on_failure = TRUE))
+}
+
+if (ci_has_env("DRAT")) {
+  get_stage("deploy") %>%
+    # remove devel version indicator to enable deployment
+    add_code_step(gsub(".9000", "", readLines("DESCRIPTION")))
+  do_drat("mlr3learners/mlr3learners.drat")
 }
